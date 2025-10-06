6777550b3fe547fe20cdcd01827d8a5b88537b84
asma <asmahashar6@gmail.com>
2025-10-06 11:20:35 -0400

    fix picture in header

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -25,9 +25,10 @@ const Navbar  = () => {
     },[])
   return (
     <>
-    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
-        <Image src={assets.header_bg_color} alt='' className='w-full'/>
+    <div className='absolute top-0 right-0 w-full -z-10'>
+  <Image src={assets.header_bg_color} alt='' className='w-full'/>
     </div>
+
       <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4
       flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm" :""}`}>
 
